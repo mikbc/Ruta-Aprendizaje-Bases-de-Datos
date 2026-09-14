cf9a0d153ede773fd9d90d24ff4688233b2a4ec3
Micaela Bell Casaubon <mbell4@palermo.edu>
2026-09-14 16:12:15 -0300

    Add procedures and functions for price and credit queries

--- Ejercicios/4° Guía/Respuestas.sql
+++ Ejercicios/4° Guía/Respuestas.sql
@@ -88,7 +88,99 @@ EXCEPTION
 END LISTA_EMP;
 
 -- 4)
+-- PROCEDIMIENTO
+create or replace PROCEDURE CONSULTA_PRECIO 
+(
+  PI_PRODUCT_ID IN PRODUCT.PRODUCT_ID%TYPE 
+, PO_LIST_PRICE OUT PRICE.LIST_PRICE%TYPE 
+, PO_MIN_PRICE OUT PRICE.MIN_PRICE%TYPE 
+) AS 
+    
+BEGIN
+  SELECT list_price, min_price
+    INTO po_list_price, po_min_price
+    FROM price
+    WHERE product_id = pi_product_id
+        AND end_date IS NULL;
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        po_list_price := NULL;
+        po_min_price := NULL;
+        dbms_output.put_line('ERROR: No se encontró un producto con ID '||pi_product_id);
+    WHEN OTHERS THEN
+        po_list_price := NULL;
+        po_min_price := NULL;
+        dbms_output.put_line('ERROR INESPERADO ('||SQLCODE||'): '||SQLERRM);
+END CONSULTA_PRECIO;
+
+-- BLOQUE ANÓNIMO
+SET SERVEROUTPUT ON
+DECLARE 
+    v_list_price price.list_price%TYPE;
+    v_min_price price.min_price%TYPE;
+BEGIN
+    consulta_precio(100860, v_list_price, v_min_price);
+    IF v_list_price IS NOT NULL THEN
+        dbms_output.put_line('- Precio de lista: $'||v_list_price); -- - Precio de lista: $35
+        dbms_output.put_line('- Precio mínimo: $'||v_min_price); -- - Precio mínimo: $28
+    END IF;
+END;
+
 -- 5)
+-- FUNCIÓN
+CREATE OR REPLACE FUNCTION Q_CREDIT 
+(
+  PI_CUSTOMER_ID IN CUSTOMER.CUSTOMER_ID%TYPE 
+) RETURN CUSTOMER.CREDIT_LIMIT%TYPE AS 
+
+    v_credit_limit customer.credit_limit%TYPE;
+    
+BEGIN
+    SELECT credit_limit
+    INTO v_credit_limit
+    FROM customer
+    WHERE customer_id = pi_customer_id;
+    
+  RETURN v_credit_limit;
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        RETURN NULL;
+        dbms_output.put_line('ERROR: No existe un cliente con ID '||pi_customer_id);
+    WHEN VALUE_ERROR THEN
+        RETURN NULL;
+        dbms_output.put_line('ERROR: El valor ingresado como ID es inválido.');
+    WHEN OTHERS THEN
+        dbms_output.put_line('ERROR INESPERADO ('||SQLCODE||'): '||SQLERRM);
+END Q_CREDIT;
+
+-- BLOQUE ANÓNIMO
+SET SERVEROUTPUT ON
+DECLARE
+    v_limite customer.credit_limit%TYPE;
+BEGIN
+    v_limite := Q_CREDIT(100);
+    dbms_output.put_line('Límite de crédito: '||NVL(TO_CHAR(v_limite), 'Cliente inexistente'));
+END;
+
 -- 6)
+CREATE OR REPLACE FUNCTION Valida_Loc (
+    p_codigo IN location.location_id%TYPE
+)
+RETURN BOOLEAN
+IS
+    v_location location.location_id%TYPE;
+BEGIN
+    SELECT location_id
+    INTO v_location
+    FROM location
+    WHERE location_id = p_codigo;
+
+    RETURN TRUE;
+
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        RETURN FALSE;
+END;
+
 -- 7)
 -- 8)
